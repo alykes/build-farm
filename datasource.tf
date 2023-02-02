71d404a4ff9a30c93c8339cc1c79b828b60ee48b
alykes <70679748+alykes@users.noreply.github.com>
2023-02-02 15:30:39 +1100

    Update datasource.tf

--- datasource.tf
+++ datasource.tf
@@ -1,4 +1,4 @@
 data "external" "wfh_public_ip" {
-  program = ["cmd", "/C", "curl -s https://ipinfo.io/json"]
-  #program = ["sh","-c", "curl -s https://ipinfo.io/json" ]
+  program = ["cmd", "/C", "curl -s https://ipinfo.io/json --ssl-no-revoke"]
+  #program = ["sh","-c", "curl -s https://ipinfo.io/json --ssl-no-revoke" ]
 }
